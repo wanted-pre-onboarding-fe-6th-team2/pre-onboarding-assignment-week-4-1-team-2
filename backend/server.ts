4761b0b45424a8d504f5aa6ea22bba981a07c704
HaJunRyu <hjr4557@naver.com>
2022-09-20 21:39:38 +0900

    Env: yarn workspaces에 backend 패키지를 추가한다

---
+++ backend/server.ts
@@ -0,0 +1,23 @@
+const jsonServer = require('json-server');
+
+const auth = require('json-server-auth');
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+app.db = router.db;
+const rules = auth.rewriter({
+  users: 660,
+  userSetting: 660,
+  accounts: 660,
+});
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+
+const JSON_SERVER_PORT = 4000;
+
+app.listen(JSON_SERVER_PORT, () => {
+  console.log(`JSON Server is running to http://localhost:${JSON_SERVER_PORT}`);
+});
