19f9bfef325f5c522e0310b1d13a2cd0527692f1
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-22 08:04:07 +0900

    Feat: Login Page 설정 및 라우트 설정
    - 라우트 상수화 처리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
+import { ROUTES } from '@/constants/routes';
 import resetCss from '@/styles/reset';
 import Home from '@/pages/Home/Home';
+import Login from '@/pages/Login/Login';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.LOGIN} element={<Login />} />
     </Routes>
   </div>
 );
---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/LoginForm/LoginForm';
+
+const Login = () => {
+  return (
+    <>
+      <h1>PREFACE</h1>
+      <h2>로그인</h2>
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
