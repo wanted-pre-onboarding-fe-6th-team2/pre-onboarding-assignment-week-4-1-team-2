07acf975fbbacacb64a51164971b728730ebaea8
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-21 17:22:15 +0900

    Feature: Sidebar 크기조정 기능 구현

--- frontend/src/components/common/Layout/Layout.jsx
+++ frontend/src/components/common/Layout/Layout.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
 import Header from '@/components/common/Layout/Header/Header';
 import Footer from '@/components/common/Layout/Footer/Footer';
 import Sidebar from '@/components/common/Layout/Sidebar/Sidebar';
 
-const Layout = ({ children }) => (
-  <Grid
-    templateAreas={`"nav header"
+const Layout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Grid
+      templateAreas={`"nav header"
                   "nav main"
                   "nav footer"`}
-    gridTemplateRows="50px 1fr 50px"
-    gridTemplateColumns="150px 1fr"
-    h="100vh"
-    color="blackAlpha.700"
-    fontWeight="bold"
-  >
-    <Header />
-    <Sidebar />
-    <GridItem pl="2" bg="green.300" area="main">
-      {children}
-    </GridItem>
-    <Footer />
-  </Grid>
-);
+      gridTemplateRows="60px 1fr 50px"
+      gridTemplateColumns={`${0.4 - 0.3 * collapsed}fr 3fr`}
+      h="100vh"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <Header collapsed={collapsed ? 1 : 0} setCollapsed={setCollapsed} />
+      <Sidebar collapsed={collapsed ? 1 : 0} />
+      <GridItem p="2" bg="gray.100" area="main">
+        {children}
+      </GridItem>
+      <Footer />
+    </Grid>
+  );
+};
 
 export default Layout;
