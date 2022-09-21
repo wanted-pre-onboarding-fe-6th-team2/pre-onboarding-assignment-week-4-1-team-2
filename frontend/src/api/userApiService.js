36bbcb7b0d400e04fec38fabdb5758cc5efe7276
HaJunRyu <hjr4557@naver.com>
2022-09-22 01:15:54 +0900

    Feat: userApiService를 구현한다

--- frontend/src/api/userApiService.js
+++ frontend/src/api/userApiService.js
@@ -1,7 +1,83 @@
+import { v4 as uuidv4 } from 'uuid';
 import http from '@/api/core';
 
 const userApiService = {
-  login: ({ email, password } = {}) => http.post({ url: '/login', data: { email, password } }),
+  getUsersCount: async () => {
+    const usersResponse = await http.get({ url: `/users` });
+
+    const filteredUsers = usersResponse.filter(({ uuid }) => uuid);
+
+    return filteredUsers.length;
+  },
+
+  getUsers: async ({ page, limit } = { page: 1, limit: 20 }) =>
+    http.get({ url: `/users?_page=${page}&_limit=${limit}` }),
+
+  getUser: ({ userId }) => http.get({ url: `/users/${userId}` }),
+
+  async createUser({
+    profilePhotoUrl,
+    name,
+    email,
+    password,
+    age,
+    genderOrigin,
+    birthDate,
+    phoneNumber,
+    address,
+    detailAddress,
+  }) {
+    const currentDateISOString = new Date().toISOString();
+    const uuid = uuidv4();
+    const createUserResponse = await http.post({
+      url: '/users',
+      data: {
+        uuid,
+        photo: profilePhotoUrl,
+        name,
+        email,
+        password,
+        age,
+        gender_origin: genderOrigin,
+        birth_date: birthDate,
+        phone_number: phoneNumber,
+        address,
+        detail_address: detailAddress,
+        last_login: currentDateISOString,
+        created_at: currentDateISOString,
+        updated_at: currentDateISOString,
+      },
+    });
+
+    const createUserSettingResponse = await http.post({
+      url: '/userSetting',
+      data: {
+        uuid,
+        allow_marketing_push: false,
+        allow_invest_push: false,
+        is_active: true,
+        is_staff: false,
+        created_at: currentDateISOString,
+        updated_at: currentDateISOString,
+      },
+    });
+
+    return { ...createUserResponse, ...createUserSettingResponse };
+  },
+
+  deleteUser: async ({ userUuid }) => {
+    const [deleteTargetUser] = await http.get({ url: `users?q=${userUuid}` });
+    const [deleteTargetUserSetting] = await http.get({ url: `userSetting?q=${userUuid}` });
+
+    if (!deleteTargetUser || !deleteTargetUserSetting)
+      throw new Error('삭제하려는 유저의 식별자를 찾지 못했습니다.');
+
+    http.delete({ url: `users/${deleteTargetUser.id}` });
+    http.delete({ url: `userSetting/${deleteTargetUserSetting.id}` });
+  },
+
+  changeUserName: ({ userId, userName }) =>
+    http.patch({ url: `users/${userId}`, data: { name: userName } }),
 };
 
 export default userApiService;
