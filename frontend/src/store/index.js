4c100d7e2ee1c6e761e0dad62d330998c365768e
HaJunRyu <hjr4557@naver.com>
2022-09-22 19:21:48 +0900

    Feat: redux devtools에 대한 옵션을 추가한다

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '@/store/modules/auth';
 
+const { MODE } = import.meta.env;
+
 const store = configureStore({
   reducer: {
     auth: authReducer,
   },
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
+  devTools: MODE === 'development',
 });
 
 export default store;
