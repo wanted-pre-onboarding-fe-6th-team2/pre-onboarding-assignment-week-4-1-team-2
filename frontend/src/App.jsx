8d8812c5ca0d8e06686c1a63ec3ac05d4a46ac7b
Namkyung <srirachacho@gmail.com>
2022-09-24 01:16:39 +0900

    Feat: 사용자 리스트 라우터 연결 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { ROUTES } from '@/constants/routes';
 import resetCss from '@/styles/reset';
 import Home from '@/pages/Home/Home';
 import Login from '@/pages/Login/Login';
+import UsersList from '@/pages/UsersList/UsersList';
 
 const App = () => (
   <ChakraProvider>
@@ -12,6 +13,7 @@ const App = () => (
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
+      <Route path={ROUTES.USERSLIST} element={<UsersList />} />
     </Routes>
   </ChakraProvider>
 );
