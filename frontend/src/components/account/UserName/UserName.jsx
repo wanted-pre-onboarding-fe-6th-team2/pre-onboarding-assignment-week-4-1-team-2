52eac8cb16a60a1a880d8e5e91544ed42ad6e62f
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-24 01:51:50 +0900

    Refactor: useAccountUser 활용해 로직 분리

--- frontend/src/components/account/UserName/UserName.jsx
+++ frontend/src/components/account/UserName/UserName.jsx
@@ -1,18 +1,10 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import userApiService from '@/api/userApiService';
 import { ROUTES } from '@/constants/routes';
+import { useAccountUser } from '@/hooks/useAccountUser';
 
 const UserName = ({ userId }) => {
-  const [userName, setUserName] = useState('');
-
-  const getUserName = useCallback(async () => {
-    const response = await userApiService.getUser({ userId });
-
-    setUserName(response.name);
-  }, [userId]);
-
-  if (userId) getUserName();
+  const userName = useAccountUser(userId);
 
   return <Link to={`${ROUTES.USER}/${userId}`}>{userName}</Link>;
 };
