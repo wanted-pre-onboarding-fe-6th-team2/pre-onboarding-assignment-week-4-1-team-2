39ac3da949d492769ab987426de43a83e05f6db4
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-23 09:52:52 +0900

    Feat: Account Detail,  User Detail 페이지 라우트 추가
    - 유지보수와 효율성을 위해 라우트 문자열 상수화 처리
    - URL params의 id를 기반으로 라우트 처리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
 import resetCss from '@/styles/reset';
 import Home from '@/pages/Home/Home';
+import AccountDetail from '@/pages/AccountDetail/AccountDetail';
+import UserDetail from '@/pages/UserDetail/UserDetail';
+import { ROUTES } from '@/constants/routes';
 
 const App = () => (
   <div>
     <Global styles={resetCss} />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={`${ROUTES.ACCOUNT_DETAIL}/:accountId`} element={<AccountDetail />} />
+      <Route path={`${ROUTES.USER_DETAIL}/:userId`} element={<UserDetail />} />
     </Routes>
   </div>
 );
---
+++ frontend/src/constants/routes.js
@@ -0,0 +1,6 @@
+export const ROUTES = {
+  HOME: '/',
+  LOGIN: '/login',
+  USER_DETAIL: '/user',
+  ACCOUNT_DETAIL: '/account',
+};
