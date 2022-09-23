8eb0ec10297a3a609c134fe4c8b30380d63c28fd
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-23 13:44:34 +0900

    Feat: Route, 컴포넌트 네이밍 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { Global } from '@emotion/react';
 import { Route, Routes } from 'react-router-dom';
 import resetCss from '@/styles/reset';
 import Home from '@/pages/Home/Home';
-import AccountDetail from '@/pages/AccountDetail/AccountDetail';
-import UserDetail from '@/pages/UserDetail/UserDetail';
+import Account from '@/pages/Account/Account';
+import User from '@/pages/User/User';
 import { ROUTES } from '@/constants/routes';
 
 const App = () => (
@@ -11,8 +11,8 @@ const App = () => (
     <Global styles={resetCss} />
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
-      <Route path={`${ROUTES.ACCOUNT_DETAIL}/:accountId`} element={<AccountDetail />} />
-      <Route path={`${ROUTES.USER_DETAIL}/:userId`} element={<UserDetail />} />
+      <Route path={`${ROUTES.ACCOUNT}/:accountId`} element={<Account />} />
+      <Route path={`${ROUTES.USER}/:userId`} element={<User />} />
     </Routes>
   </div>
 );
