318c5282973bb1a7150375a6d65801e91f1374ec
Namkyung <srirachacho@gmail.com>
2022-09-24 03:44:25 +0900

    Feat: 사용자 생성 폼에 chakra-ui 적용

--- frontend/src/components/UsersListBoard/UserForm.jsx
+++ frontend/src/components/UsersListBoard/UserForm.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import { Box, FormLabel, Input, Button, Select } from '@chakra-ui/react';
 import userApiService from '@/api/userApiService';
 
 const UserForm = () => {
@@ -51,105 +52,119 @@ const UserForm = () => {
     }));
   };
   return (
-    <form onSubmit={submitFormHandler}>
-      <div>
-        <label htmlFor="profilePhotoUrl">프로필 URL: </label>
-        <input
-          onChange={userInputHandler}
-          type="text"
-          id="profilePhotoUrl"
-          required
-          value={userInput.profilePhotoUrl}
-        />
-      </div>
-      <div>
-        <label htmlFor="name">이름: </label>
-        <input onChange={userInputHandler} type="text" id="name" required value={userInput.name} />
-      </div>
-      <div>
-        <label htmlFor="email">이메일: </label>
-        <input
-          onChange={userInputHandler}
-          type="text"
-          id="email"
-          required
-          value={userInput.email}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">비밀번호: </label>
-        <input
-          onChange={userInputHandler}
-          type="password"
-          id="password"
-          required
-          value={userInput.password}
-        />
-      </div>
-      <div>
-        <label htmlFor="age">나이: </label>
-        <input onChange={userInputHandler} type="number" id="age" required value={userInput.age} />
-      </div>
-      <div>
-        <label htmlFor="genderOrigin">주민등록번호상 성별: </label>
-        <select
-          onChange={userInputHandler}
-          id="genderOrigin"
-          required
-          value={userInput.genderOrigin}
-        >
-          <option value={1}>1</option>
-          <option value={2}>2</option>
-          <option value={3}>3</option>
-          <option value={4}>4</option>
-        </select>
-      </div>
-      <div>
-        <label htmlFor="birthDate">생년월일: </label>
-        <input
-          onChange={userInputHandler}
-          type="date"
-          id="birthDate"
-          required
-          min="1800-01-01"
-          max="2022-12-31"
-          value={userInput.birthDate}
-        />
-      </div>
-      <div>
-        <label htmlFor="phoneNumber">핸드폰 번호: </label>
-        <input
-          onChange={userInputHandler}
-          type="tel"
-          id="phoneNumber"
-          placeholder="010-0000-0000"
-          pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
-          required
-          value={userInput.phoneNumber}
-        />
-      </div>
-      <div>
-        <label htmlFor="address">주소: </label>
-        <input
-          onChange={userInputHandler}
-          type="text"
-          id="address"
-          required
-          value={userInput.address}
-        />
-      </div>
-      <div>
-        <label htmlFor="detailAddress">상세 주소: </label>
-        <input
-          onChange={userInputHandler}
-          type="text"
-          id="detailAddress"
-          required
-          value={userInput.detailAddress}
-        />
-      </div>
-      <button type="submit">생성하기</button>
-    </form>
+    <Box>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <FormLabel htmlFor="profilePhotoUrl">프로필 URL: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="text"
+            id="profilePhotoUrl"
+            required
+            value={userInput.profilePhotoUrl}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="name">이름: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="text"
+            id="name"
+            required
+            value={userInput.name}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="email">이메일: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="text"
+            id="email"
+            required
+            value={userInput.email}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="password">비밀번호: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="password"
+            id="password"
+            required
+            value={userInput.password}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="age">나이: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="number"
+            id="age"
+            required
+            value={userInput.age}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="genderOrigin">주민등록번호상 성별: </FormLabel>
+          <Select
+            onChange={userInputHandler}
+            id="genderOrigin"
+            required
+            value={userInput.genderOrigin}
+          >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+          </Select>
+        </div>
+        <div>
+          <FormLabel htmlFor="birthDate">생년월일: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="date"
+            id="birthDate"
+            required
+            min="1800-01-01"
+            max="2022-12-31"
+            value={userInput.birthDate}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="phoneNumber">핸드폰 번호: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="tel"
+            id="phoneNumber"
+            placeholder="010-0000-0000"
+            pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+            required
+            value={userInput.phoneNumber}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="address">주소: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="text"
+            id="address"
+            required
+            value={userInput.address}
+          />
+        </div>
+        <div>
+          <FormLabel htmlFor="detailAddress">상세 주소: </FormLabel>
+          <Input
+            onChange={userInputHandler}
+            type="text"
+            id="detailAddress"
+            required
+            value={userInput.detailAddress}
+          />
+        </div>
+        <Button type="submit">생성하기</Button>
+      </form>
+    </Box>
   );
 };
 
