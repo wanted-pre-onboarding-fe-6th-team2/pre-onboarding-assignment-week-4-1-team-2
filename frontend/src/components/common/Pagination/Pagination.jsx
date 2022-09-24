873c2d0ba9bb78f1791b0c4c9abaa983ca002c28
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-24 19:55:37 +0900

    Refactor: 상수를 통한 데이터 표현

--- frontend/src/components/common/Pagination/Pagination.jsx
+++ frontend/src/components/common/Pagination/Pagination.jsx
@@ -4,19 +4,23 @@ import { ChevronLeftIcon, ChevronRightIcon, ArrowLeftIcon, ArrowRightIcon } from
 import { useNavigate } from 'react-router-dom';
 import { PageButton } from './Pagination.styled';
 
-const Pagination = ({ totalPages, parms }) => {
+const FIRST_PAGE = 1;
+const PAGE_MAX = 5;
+
+const Pagination = ({ totalPages, parms, setCurrentPage }) => {
   const navigator = useNavigate();
   const { page, limit, keyword, sort, order } = parms;
-  const pages = Array(totalPages < 5 ? totalPages : 5);
+  const pages = Array(totalPages < PAGE_MAX ? totalPages : PAGE_MAX);
   const [start, setStart] = useState(page > 5 ? page - 4 : page);
 
   const setPageUrl = nextPage => {
-    navigator(`?_page=${nextPage}&_limit=${limit}&_q=${keyword}&_sort=${sort}&_order=${order}`);
+    navigator(`?_page=${nextPage}&_limit=${limit}&q=${keyword}&_sort=${sort}&_order=${order}`);
+    setCurrentPage(prev => ({ ...prev, page: nextPage }));
   };
 
   const moveFirstPage = () => {
-    setPageUrl(1);
-    setStart(1);
+    setPageUrl(FIRST_PAGE);
+    setStart(FIRST_PAGE);
   };
 
   const moveEndPage = () => {
@@ -25,9 +29,9 @@ const Pagination = ({ totalPages, parms }) => {
   };
 
   const prevPage = () => {
-    if (page - 1 < 1) return;
-    if (page === start && page !== 1) {
-      if (page - 3 <= 1) setStart(prev => prev - 1);
+    if (page - 1 < FIRST_PAGE) return;
+    if (page === start && page !== FIRST_PAGE) {
+      if (page - 3 <= FIRST_PAGE) setStart(prev => prev - 1);
       else setStart(prev => prev - 3);
     }
     setPageUrl(page - 1);
@@ -44,7 +48,9 @@ const Pagination = ({ totalPages, parms }) => {
 
   return (
     <Flex>
-      <IconButton onClick={moveFirstPage} icon={<ArrowLeftIcon h={3} w={3} />} />
+      {totalPages > PAGE_MAX && (
+        <IconButton onClick={moveFirstPage} icon={<ArrowLeftIcon h={3} w={3} />} />
+      )}
       <IconButton onClick={prevPage} icon={<ChevronLeftIcon h={6} w={6} />} />
       {pages.fill().map((_, i) => (
         <PageButton
@@ -60,7 +66,9 @@ const Pagination = ({ totalPages, parms }) => {
         </PageButton>
       ))}
       <IconButton onClick={nextPage} icon={<ChevronRightIcon h={6} w={6} />} />
-      <IconButton onClick={moveEndPage} icon={<ArrowRightIcon h={3} w={3} />} />
+      {totalPages > PAGE_MAX && (
+        <IconButton onClick={moveEndPage} icon={<ArrowRightIcon h={3} w={3} />} />
+      )}
     </Flex>
   );
 };
