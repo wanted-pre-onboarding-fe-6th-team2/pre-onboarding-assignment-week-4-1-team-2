1dae30b46f46da48bfadb34bd0ee46efb5f9a4e8
jyoon <wldbszp@gmail.com>
2022-09-24 20:24:09 +0900

    Chore: / 접근할 경우 로그인 페이지 노출

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { ROUTES } from '@/constants/routes';
 import resetCss from '@/styles/reset';
-import Home from '@/pages/Home/Home';
 import AccountList from '@/pages/AccountList/AccountList';
 import Login from '@/pages/Login/Login';
 import Account from '@/pages/Account/Account';
@@ -15,7 +14,7 @@ const App = () => (
   <ChakraProvider>
     <Global styles={resetCss} />
     <Routes>
-      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.HOME} element={<Login />} />
       <Route path={ROUTES.LOGIN} element={<Login />} />
       <Route path={ROUTES.LOGOUT} element={<Logout />} />
       <Route path={ROUTES.ACOOUNTS} element={<AccountList />} />
